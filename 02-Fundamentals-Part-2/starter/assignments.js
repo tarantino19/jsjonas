81621e7ab71e6f23af2a576fca99bb5ea61135e2
tarantino19 <tolentinored19@gmail.com>
2022-06-18 16:29:04 +0800

    arrays

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,5 +1,5 @@
 'use strict';
-/*
+
 // activating strict mode
 
 // FUNCTIONS assignment
@@ -63,4 +63,66 @@ console.log (desc1Philippines);
 console.log (desc1America);
 console.log (desc1Indonesia);
 
-*/
+//arrays assignment//
+
+const populations = [107, 350, 273, 30];
+console.log (populations.length === 4);
+
+const percentages = [
+    percentageOfWorld1 (populations [0]),
+    percentageOfWorld1 (populations [1]),
+    percentageOfWorld1 (populations [2]),
+    percentageOfWorld1 (populations [3])
+];
+
+console.log (percentages);
+
+const neighbours = [`Vietnam`, `India`, `Brunei`];
+
+neighbours.push (`Utopia`);
+console.log (neighbours);
+
+neighbours.pop ();
+console.log (neighbours);
+
+// if (neighbours !== `Germany`) {
+//     console.log (`Probably not a central European country :D`);
+// }
+
+if (!neighbours.includes (`Germany`)) {
+    console.log (`Probably not a central European country :D`);
+}
+
+console.log (neighbours.indexOf (`Vietnam`));
+neighbours [0] = `Republic of Vietnam`;
+
+//or
+
+// neighbours[neighbours.indexOf('Vietnam')] = 'Republic of Vietnam';
+
+console.log (neighbours);
+
+
+const myCountry = {
+    country: `Philippines`,
+    capital: `Manila`,
+    language: `filipino`,
+    population: 107,
+    neighbours: [`Vietnam`, `India`, `Brunei`]
+};
+
+console.log (myCountry);
+
+
+// dot vs bracket notation
+
+console.log (`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, 3 neighbouring countries ${myCountry.neighbours} and a capital called ${myCountry.capital}`);
+
+myCountry.population = (2 + myCountry.population);
+
+console.log (`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, 3 neighbouring countries ${myCountry.neighbours} and a capital called ${myCountry.capital}`);
+
+myCountry [`population`] = (myCountry [`population`] - 2);
+
+console.log (`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, 3 neighbouring countries ${myCountry.neighbours} and a capital called ${myCountry.capital}`);
+
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -2,9 +2,9 @@
 
 /*
 // ACTIVATING STRICT MODE
--  use at the beginning of the script - less accidental errors
-1 - forbid to do certain things
-2 - will create visible errors in certain situation
+//  use at the beginning of the script - less accidental errors
+// 1 - forbid to do certain things
+// 2 - will create visible errors in certain situation
 
 let hasDriversLicense = false;
 const passTest = true;
@@ -12,16 +12,16 @@ const passTest = true;
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log (`I can drive!`)
 
-const interface = 'Audio'; strict mode reserve word for future js versions
-const private = 123;
-const if = 42 because there's already an if statement in js
+// const interface = 'Audio'; strict mode reserve word for future js versions
+// const private = 123;
+// const if = 42 because there's already an if statement in js
 
 
-FUNCTIONS
-a piece of code we cna reuse over and over again, like variable wth value - function can hold one or more complete likes of code
-- use descriptive function name
-allows us to reuse code
-DRY - Don't Repeat Yourself - functions for clean code
+// FUNCTIONS
+// a piece of code we cna reuse over and over again, like variable wth value - function can hold one or more complete likes of code
+// - use descriptive function name
+// allows us to reuse code
+// DRY - Don't Repeat Yourself - functions for clean code
 
 
 function logger () {
@@ -44,9 +44,9 @@ const appleOrangeJuice = fruitProcessor (2, 4);
 console.log (appleOrangeJuice);
 
 
-FUNCTION DECLARATION AND EXPRESSIONS
+// FUNCTION DECLARATION AND EXPRESSIONS
 
-a FUNCTION is a value, we can store in a variable.
+// a FUNCTION is a value, we can store in a variable.
 
 
 // function declaration
@@ -66,7 +66,7 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2 (1991)
 console.log (age2);
 
-ARROW FUNCTIONS
+// ARROW FUNCTIONS
 
 const calcAge3 = birthYear => 2037 - birthYear;
 const age3 = calcAge3 (1991)
@@ -82,7 +82,7 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 console.log ((yearsUntilRetirement) (1991, 'Red'));
 console.log ((yearsUntilRetirement) (1980, 'Jonas'));
 
-functions calling other funtions
+// functions calling other funtions
 
 function cutPieces (fruit) {
     return fruit * 4;
@@ -98,12 +98,14 @@ function fruitProcessor (apples, oranges) {
 console.log (fruitProcessor (2, 3));
 
 
-FUNCTION REVIEW
+// FUNCTION REVIEW
 
 
 const calcAge = function (birthYear) {
 return 2037 - birthYear;
 }
+
+// below could also be? 
 
 const yearsUntilRetirement = function (birthYear, firstName)  {
     const age = calcAge (birthYear);
@@ -124,6 +126,150 @@ console.log (yearsUntilRetirement (1950, 'Mike'));
 
 
 ARRAYS
+big containers where we can throw variables
 
 
+
+// const friend1 = `Michael`;
+// const friend2 = `Steve`;  too many variables
+
+const friends =  ['Michael', 'Steven', 'Peter']; // literal syntax
+console.log (friends);
+
+const year= new Array (1991, 1984, 2008, 2020);
+
+console.log (friends [0]); //arrays are zero based - 0 1 2 3 4...
+
+console.log (friends.length); //property - not zero based
+console.log (friends [friends.length - 1]); //for the last element // we can put expression inside the array
+
+friends [2] = 'Jay';
+console.log (friends);  // an array is not a primitive vaue so we can mutate the const
+// friends = ['bob', 'alice'] wont work
+
+const firtName = `Red`;
+const red = [`firstName`, `Tarantino`, 2037 - 1993, `programmer`, friends];
+console.log (red);
+console.log (red.length);
+
+//exercise
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+    }
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+// calcAge (years);  //won't work because years is array it doesnt know what to do
+// console.log (calcAge (years));
+
+const age1 = calcAge (years [0]);
+const age2 = calcAge (years [1]);
+const age3 = calcAge (years [years.length-1]);
+console.log (age1, age2, age3);
+
+// const ages = [calcAge (years [0]), calcAge (years [1]), calcAge (years [years.length-1] ) ]; 
+const ages = [age1, age2, age3];
+console.log (ages);
+
+
+//basic array operations (methods)//
+
+const friends =  ['Michael', 'Steven', 'Peter'];
+friends.push (`Jay`);
+// const newLength = friends.push (`Jay`); // push is a function (method) for adding elements
+console.log (friends);
+// console.log (newLength);
+
+friends.unshift (`John`); //unshift adds 1 in the beginning
+console.log (friends);
+
+//remove elements
+friends.pop (); //last element - removes 'Jay'
+const popped = friends.pop (); //removes 'Peter'
+console.log (popped); //logs "Peter" in the log
+console.log (friends); //shows array
+
+friends.shift (); // remove 1 at the beginning
+console.log (friends)
+
+console.log (friends.indexOf (`Steven`)); //pass of what element number
+console.log (friends.indexOf (`Bruh`)); // hehe not there so -1 on log
+
+console.log (friends.includes (`Steven`)); // includes shows true or false
+console.log (friends.includes (`Bobbbyy`));
+
+friends.push (23); 
+console.log (friends.includes ('23')); // false cause number then string
+
+console.log (friends);
+console.log (friends.includes (23)); // will be true
+
+//conditionals
+
+// if (friends.includes (`Peter`)) {
+//     console.log (`You have a friend called Peter`); //nothing happens in the log
+// } 
+
+if (friends.includes (`Steven`)) {
+    console.log (`You have a friend called Steven`);
+}  //it shows `You have a friend called Steven` in the log
+
+
+//objects allows us to NAME or define key value pairs.
+// [] square for arrays, {} curly for objects
+
+// object literal syntax - for grouping different variables that belongs together - object, order values doesnt matter
+
+const red = {
+    firstName: `Red`,
+    lastName: `Tarantino`,
+    age: 2022 - 1993,
+    job: `programmer`,
+    friends:  [`Gian`, `James`, `Ken`]
+};
+
+//DOT VS BRACKET NOTATION
+
+const red = {
+    firstName: `Red`,
+    lastName: `Tarantino`,
+    age: 2022 - 1993,
+    job: `programmer`,
+    friends:  [`Gian`, `James`, `Ken`]
+};
+console.log (red);
+
+console.log (red.lastName);
+console.log (red [`lastName`]);
+
+const nameKey = `Name`;
+console.log (red [`first` + nameKey]);
+console.log (red [`last` + nameKey]);
+
+// console.log (red. `last` + nameKey]);)  would not work,we need real property name
+// when to use BRACKET? example:
+
+const interestedIn = prompt (`What do you want to know about Red? Choose between firstName, lastName, age, job, and friends`);
+
+// console.log (red [interestedIn]);
+
+if  (red [interestedIn]) {
+    console.log (red [interestedIn]);
+} else {
+    console.log (`info not available. Choose between firstName, lastName, age, job, and friends`);
+}
+
+//adding new properties
+red.location = `Pilipinas`; // can be    red [`Pilipinas`];
+red [`twitter`] = `@redify`; // can be  red.twitter = `@redify`;
+console.log (red);
+
+// Challenge
+
+// "Jonas has 3 friends, and his best friend is called Gian"
+
+console.log (`${red.firstName} has ${red.friends.length} friends, and his best friend is called ${red.friends [0]}`);
+
+// dot and bracket are operators. - this works cause of operator precedence
 */
